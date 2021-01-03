a09ed62ba421cc40196cac645848d8cd443e149c
Mertlin <34924612+Mertlin@users.noreply.github.com>
2021-01-03 15:24:31 +0100

    Nav Dropdown changed to JS Function
    Necessary because Dropdown didnt open on hover on mobile devices.

---
+++ src/prototypes/nav.js
@@ -0,0 +1,8 @@
+function toggleDD() {
+    var dd = document.getElementById('nav-dd-wrapper');
+    if(dd.style.display=="flex") {
+        dd.style.display="none";
+    } else {
+        dd.style.display="flex";
+    }
+}
