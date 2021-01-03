7db1d86f526adefccf8e53977177c80837e04531
Mertlin <34924612+Mertlin@users.noreply.github.com>
2021-01-03 11:42:58 +0100

    Vieles
    Geschichte aus Nav entfernt -> zu viel arbeit bringt sich aber wenig.
    
    Beiträge und Termine für mobil optimiert

--- src/prototypes/sidebar.js
+++ src/prototypes/sidebar.js
@@ -20,18 +20,24 @@ function select(className) {
         toHide[i].style.display = 'none';
     }
     for (i = 0; i < toShow.length; i++) {
-        toShow[i].style.display = 'block';
+        if (toShow[i].classList.contains('termin')) {
+            toShow[i].style.display = 'flex';
+        } else {
+            toShow[i].style.display = 'block';
+        }
     }
     backArrow.style.display = 'block';
     if (!toShow[0].classList.contains("active")) {
         toShow[0].classList.add("active");
+        backArrow.classList.add("active");
         toShow[0].classList.add("active-border");
     }
 
-    /* Hide aktuelle Beiträge (nur für Beitraege.html)*/
+    /* Hide aktuelle Beiträge oder Termine (nur für Beitraege.html)*/
     var toHideContent = document.getElementsByClassName('aktuell');
     for( i = 0; i < toHideContent.length;i++){
-        toHideContent[i].style.display = 'none';
+        if (!toHideContent[i].classList.contains(className))
+            toHideContent[i].style.display = 'none';
     }
 }
 
@@ -55,13 +61,17 @@ function back() {
         toHideBar[i].style.display = 'none';
     }
 
-    /* Nur für beitraege.html benötigt */
-    var toHideContent = document.querySelectorAll('.beitrag, .category-header');
+    /* Nur für beitraege und termine benötigt */
+    var toHideContent = document.querySelectorAll('.beitrag, .termin, .category-header, .termin-header');
     for( i = 0; i < toHideContent.length;i++){
         toHideContent[i].style.display = 'none';
     }
     var toShowContent = document.getElementsByClassName('aktuell');
     for( i = 0; i < toShowContent.length;i++){
-        toShowContent[i].style.display = 'block';
+        if (toShowContent[i].classList.contains('termin')) {
+            toShowContent[i].style.display = 'flex';
+        } else {
+            toShowContent[i].style.display = 'block';
+        }
     }
 }
